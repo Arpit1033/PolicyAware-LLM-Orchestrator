261da947b8980fe13ae25e03b539e796ac96eca5
Arpit1033 <dubey1033arpit@gmail.com>
2026-04-16 10:18:32 +0530

    chore(security): secure Django SECRET_KEY with decouple - Removed hardcoded SECRET_KEY from core/settings.py. - Configured python-decouple to load SECRET_KEY from .env file to prevent leakage in version control.

--- src/core/settings.py
+++ src/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ym-dfg@+0-2dko*abvf*x1c6t$@@#mb%h@u9+k+iq9bdg$5ink"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
