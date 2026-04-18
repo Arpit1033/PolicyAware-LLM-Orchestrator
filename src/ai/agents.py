78f13fd7530aa664f1852fd43a4c8e4eb12142b3
Arpit1033 <dubey1033arpit@gmail.com>
2026-04-18 11:17:57 +0530

    feat(ai): orchestrate agents with langgraph_supervisor logic

--- src/ai/agents.py
+++ src/ai/agents.py
@@ -1,3 +1,4 @@
+from langchain_core.messages import SystemMessage, HumanMessage
 from langgraph.prebuilt import create_react_agent
 
 from ai.llms import get_llm_model
@@ -6,13 +7,31 @@ from ai.tools import (
     movie_discovery_tools
 )
 
+def document_agent_modifier(state):
+    messages = state.get("messages", [])
+    out_messages = [SystemMessage(content="You are a helpful assistant that manages documents.")] + messages
+
+    if messages and not isinstance(messages[-1], HumanMessage):
+        out_messages.append(HumanMessage(content="System routed me to you. Please execute the tool required for my initial request."))
+        
+    return out_messages
+
+def movie_agent_modifier(state):
+    messages = state.get("messages", [])
+    out_messages = [SystemMessage(content="You are a helpful assistant in finding and discovering information about movies.")] + messages
+    
+    if messages and not isinstance(messages[-1], HumanMessage):
+        out_messages.append(HumanMessage(content="System routed me to you. Please execute the tool required for my initial request."))
+        
+    return out_messages
+
 def get_document_agent(model=None, checkpointer=None):
     llm_model = get_llm_model(model=model)
 
     agent = create_react_agent(
         model=llm_model,  
         tools=document_tools,  
-        prompt="You are a helpful assistant in managing a user's documents within this app.",
+        prompt=document_agent_modifier,
         checkpointer=checkpointer,
         name="document-assistant"
     )
@@ -24,7 +43,7 @@ def get_movie_discovery_agent(model=None, checkpointer=None):
     agent = create_react_agent(
         model=llm_model,  
         tools=movie_discovery_tools,  
-        prompt="You are a helpful assistant in finding and discovering information about movies.",
+        prompt=movie_agent_modifier,
         checkpointer=checkpointer,
         name="movie-discovery-assistant"
     )
---
+++ src/ai/supervisors.py
@@ -0,0 +1,19 @@
+from langgraph_supervisor import create_supervisor
+
+from ai import agents
+from ai.llms import get_llm_model
+
+def get_supervisor(model=None, checkpointer=None):
+    llm_model = get_llm_model(model=model)
+
+    return create_supervisor(
+        agents=[
+            agents.get_document_agent(),
+            agents.get_movie_discovery_agent(),
+        ],
+        model=llm_model,
+        prompt=(
+            "You manage a document management assistant and a "
+            "movie discovery assistant. Assign work to them."
+        ),
+    ).compile(checkpointer=checkpointer)
