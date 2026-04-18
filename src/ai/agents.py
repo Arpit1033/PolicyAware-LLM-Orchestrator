7db371e6dfcff9ca4d632933c3ef16b3d17fb5d1
Arpit1033 <dubey1033arpit@gmail.com>
2026-04-18 10:36:52 +0530

    feat(ai): integrate movie discovery tools and agent

--- src/ai/agents.py
+++ src/ai/agents.py
@@ -1,7 +1,10 @@
 from langgraph.prebuilt import create_react_agent
 
 from ai.llms import get_llm_model
-from ai.tools.documents import document_tools
+from ai.tools import (
+    document_tools,
+    movie_discovery_tools
+)
 
 def get_document_agent(model=None, checkpointer=None):
     llm_model = get_llm_model(model=model)
@@ -12,5 +15,17 @@ def get_document_agent(model=None, checkpointer=None):
         prompt="You are a helpful assistant in managing a user's documents within this app.",
         checkpointer=checkpointer,
         name="document-assistant"
+    )
+    return agent
+
+def get_movie_discovery_agent(model=None, checkpointer=None):
+    llm_model = get_llm_model(model=model)
+
+    agent = create_react_agent(
+        model=llm_model,  
+        tools=movie_discovery_tools,  
+        prompt="You are a helpful assistant in finding and discovering information about movies.",
+        checkpointer=checkpointer,
+        name="movie-discovery-assistant"
     )
     return agent
